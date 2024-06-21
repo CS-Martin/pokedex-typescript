442ce2703ff8a6210ef31d85afb7a05494dc0a70
Martin <meatole.cs@gmail.com>
2024-06-21 19:31:15 +0800

    feat: search ui

--- src/components/custom-components/header.tsx
+++ src/components/custom-components/header.tsx
@@ -1,15 +1,20 @@
 import Image from "next/image";
+import { ModeToggle } from "./theme-toggler";
+import { Search } from 'lucide-react';
 
 const Header = () => {
     return (
-        <nav className="absolute w-full h-[56px] flex items-center justify-between">
+        <nav className="absolute w-full h-[100px] flex items-center justify-between">
             <div>
                 <Image src={'/pokedex-logo.png'} alt="Pokedex Logo" width={100} height={100} />
             </div>
 
-            <div>
-                Search
-                Theme
+            <div className="flex gap-x-7 items-center">
+                <a href="" className="flex gap-x-2 items-center">
+                    <Search size={18} />
+                    <small className="font-semibold text-[12px] rounded-md p-1">⌘K</small>
+                </a>
+                <ModeToggle />
             </div>
         </nav>
     );
