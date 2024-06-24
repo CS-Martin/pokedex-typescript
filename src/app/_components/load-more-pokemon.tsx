d4cbd0224ebbc24dc48f1ad0f12e43a7c1110a51
Martin <meatole.cs@gmail.com>
2024-06-24 17:18:36 +0800

    feat: improved ui of button indicator

--- src/app/_components/load-more-pokemon.tsx
+++ src/app/_components/load-more-pokemon.tsx
@@ -7,15 +7,15 @@ type LoadMorePokemonButtonProps = {
 
 const LoadMorePokemonButton: React.FC<LoadMorePokemonButtonProps> = ({ onClick }) => {
     return (
-        <Button 
+        <Button
             className="w-full text-[15px] rounded-full py-6 bottom-nav-container flex items-center justify-start relative group"
             onClick={onClick}
         >
-            <ArrowRight 
-                size={18} 
-                className="absolute left-0 ml-4 opacity-0 transition-opacity duration-500 ease-in-out group-hover:opacity-100 group-hover:ml-4" 
+            <ArrowDown
+                size={24}
+                className="absolute animate-bounce left-0 ml-4 opacity-0 transition-opacity duration-500 ease-in-out group-hover:opacity-100 group-hover:ml-4"
             />
-            <span className="transition-transform duration-500 ease-in-out group-hover:translate-x-5">
+            <span className="transition-transform duration-500 ease-in-out group-hover:translate-x-6">
                 Load More Pokemon
             </span>
         </Button>
